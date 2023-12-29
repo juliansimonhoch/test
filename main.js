a982e037dd25cdb3867b92bc0ca9361f9a84e1f0
eroehner <e.roehner@icloud.com>
2023-12-29 02:52:51 +0100

    Initial Release

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 //IMPORT MODULES
-import './style.css';
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import GUI from 'lil-gui';
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import GUI, { Controller } from "lil-gui";
 
 //CONSTANT & VARIABLES
 let width = window.innerWidth;
@@ -10,16 +10,29 @@ let height = window.innerHeight;
 
 //-- GUI PARAMETERS
 var gui;
+var widthController;
+var heightController;
+var depthController;
 const parameters = {
-  Board() { createBoard()},
-  Beam() { createBeam()},
-  Bar() { createBar()},
-  test() { hi() },
-}
+  Board() {
+    createBoard();
+  },
+  Beam() {
+    createBeam();
+  },
+  Bar() {
+    createBar();
+  },
+  test() {
+    hi();
+  },
+  width: 0,
+  height: 0,
+  depth: 0,
+};
 
-
-function hi(){
-  alert( 'Booom, das funktioniert schonmal!' );
+function hi() {
+  alert("Booom, das funktioniert schonmal!");
 }
 
 //-- SCENE VARIABLES
@@ -31,6 +44,13 @@ var control;
 var ambientLight;
 var directionalLight;
 
+//-- RAYCASTER VARIABLES
+let group;
+let selectedObject = null;
+const raycaster = new THREE.Raycaster();
+const pointer = new THREE.Vector2();
+var transformedObject = null;
+
 //-- GEOMETRY PARAMETERS
 //Create an empty array for storing all the cubes
 let sceneObjects = [];
@@ -39,147 +59,178 @@ let boardCounter = 1;
 let beamCounter = 1;
 let barCounter = 1;
 
-function main(){
+function main() {
   //GUI
-  gui = new GUI;
-  gui.add(parameters, 'test');
+  gui = new GUI();
+  gui.add(parameters, "test");
   //Add Objects
-  const addObjectFolder = gui.addFolder('Add Objects');
-  addObjectFolder.add(parameters, 'Board');
-  addObjectFolder.add(parameters, 'Beam');
-  addObjectFolder.add(parameters, 'Bar');
-  
+  const addObjectFolder = gui.addFolder("Add Objects");
+  addObjectFolder.add(parameters, "Board");
+  addObjectFolder.add(parameters, "Beam");
+  addObjectFolder.add(parameters, "Bar");
+  //Add Object Settings
+  const selectedObjectFolder = gui.addFolder("Selected Object");
+  widthController = selectedObjectFolder.add(parameters, "width");
+  heightController = selectedObjectFolder.add(parameters, "height");
+  depthController = selectedObjectFolder.add(parameters, "depth");
+
   //CREATE SCENE AND CAMERA
   scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera( 15, width / height, 0.1, 1000);
-  camera.position.set(200, 100, 170)
+  camera = new THREE.PerspectiveCamera(15, width / height, 0.1, 1000);
+  camera.position.set(200, 100, 170);
 
   //LIGHTINGS
   ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
   scene.add(ambientLight);
 
-  directionalLight = new THREE.DirectionalLight( 0xffffff, 1);
-  directionalLight.position.set(2,5,5);
-  directionalLight.target.position.set(-1,-1,0);
-  scene.add( directionalLight );
+  directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+  directionalLight.position.set(2, 5, 5);
+  directionalLight.target.position.set(-1, -1, 0);
+  scene.add(directionalLight);
   scene.add(directionalLight.target);
 
   //GEOMETRY INITIATION
   // Initiate first cubes
   createGrid();
 
-  //RESPONSIVE WINDOW
-  window.addEventListener('resize', handleResize);
- 
   //CREATE A RENDERER
-  renderer = new THREE.WebGLRenderer({alpha:true, antialias:true});
-  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-  container = document.querySelector('#threejs-container');
+  container = document.querySelector("#threejs-container");
+  let canvas = renderer.domElement;
   container.append(renderer.domElement);
-  
+
   //CREATE MOUSE CONTROL
-  control = new OrbitControls( camera, renderer.domElement );
+  control = new OrbitControls(camera, renderer.domElement);
+
+  //RESPONSIVE WINDOW
+  window.addEventListener("resize", handleResize);
+  document.addEventListener("pointermove", onPointerMove);
+  canvas.addEventListener("click", onClick);
+  window.addEventListener("keydown", onKey);
+
+  //RAYCASTER
+  group = new THREE.Group();
+  scene.add(group);
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.minDistance = 15;
+  controls.maxDistance = 250;
+
+  //Transform selected Objects
+  widthController.onChange(function (v) {
+    transformedObject.scale.x = scaleFactor(
+      parameters.width,
+      transformedObject.geometry.parameters.width
+    );
+  });
+
+  heightController.onChange(function (v) {
+    transformedObject.scale.y = scaleFactor(
+      parameters.height,
+      transformedObject.geometry.parameters.height
+    );
+  });
+
+  depthController.onChange(function (v) {
+    transformedObject.scale.z = scaleFactor(
+      parameters.depth,
+      transformedObject.geometry.parameters.depth
+    );
+  });
 
   //EXECUTE THE UPDATE
   animate();
 }
- 
+
 //-----------------------------------------------------------------------------------
 //HELPER FUNCTIONS
 //-----------------------------------------------------------------------------------
 //GEOMETRY FUNCTIONS
 
-
 // Create Grid
-function createGrid(){
-  scene.add( new THREE.GridHelper( 100, 100, 0xd3d3d3 , 0xd3d3d3 ) );
-  scene.add( new THREE.GridHelper( 100, 10, 0x151515 , 0x151515 ) );
+function createGrid() {
+  scene.add(new THREE.GridHelper(100, 100, 0xd3d3d3, 0xd3d3d3));
+  scene.add(new THREE.GridHelper(100, 10, 0x151515, 0x151515));
 }
 
 //BOARDS
 //create Boards
-function createBoard(){
+function createBoard() {
   const geometry = new THREE.BoxGeometry(40, 2, 40);
   const material = new THREE.MeshPhysicalMaterial();
-  material.color = new THREE.Color(0x7BAFD4);
+  material.color = new THREE.Color("#69f");
 
   const board = new THREE.Mesh(geometry, material);
   board.position.set(0, 1, 0);
   board.name = "board " + boardCounter;
-  boardCounter ++;
+  boardCounter++;
   sceneObjects.push(board);
 
   scene.add(board);
+  group.add(board);
   animate();
 }
 
 //Transform Boards
-function transformBoard(){
-
-}
-
+function transformBoard() {}
 
 //Beams
 //create Beams
-function createBeam(){
+function createBeam() {
   const geometry = new THREE.BoxGeometry(40, 2, 2);
   const material = new THREE.MeshPhysicalMaterial();
-  material.color = new THREE.Color(0x7BAFD4);
+  material.color = new THREE.Color("#69f");
 
   const beam = new THREE.Mesh(geometry, material);
   beam.position.set(0, 1, 0);
   beam.name = "beam " + beamCounter;
-  beamCounter ++;
+  beamCounter++;
   sceneObjects.push(beam);
 
   scene.add(beam);
+  group.add(beam);
   animate();
 }
 
 //Transform Beams
-function transformBeam(){
-
-}
-
+function transformBeam() {}
 
 //BARS
 //create Bars
-function createBar(){
-  const geometry = new THREE.CylinderGeometry( 1, 1, 40, 32 );
+function createBar() {
+  const geometry = new THREE.CylinderGeometry(1, 1, 40, 32);
   const material = new THREE.MeshPhysicalMaterial();
-  material.color = new THREE.Color(0x7BAFD4);
+  material.color = new THREE.Color("#69f");
 
   const bar = new THREE.Mesh(geometry, material);
   bar.position.set(0, 1, 0);
-  bar.rotation.set(90 * (Math.PI/180), 0, 90 * (Math.PI/180));
+  bar.rotation.set(90 * (Math.PI / 180), 0, 90 * (Math.PI / 180));
   bar.name = "bar " + beamCounter;
-  barCounter ++;
+  barCounter++;
   sceneObjects.push(bar);
 
   scene.add(bar);
+  group.add(bar);
   animate();
 }
 
 //Transform Bars
-function transformBar(){
-
-}
-
+function transformBar() {}
 
 //Remove 3D Objects and clean the caches
-function removeObject(sceneObject){
+function removeObject(sceneObject) {
   if (!(sceneObject instanceof THREE.Object3D)) return;
 
   //Remove the geometry to free GPU resources
-  if(sceneObject.geometry) sceneObject.geometry.dispose();
+  if (sceneObject.geometry) sceneObject.geometry.dispose();
 
   //Remove the material to free GPU resources
-  if(sceneObject.material){
+  if (sceneObject.material) {
     if (sceneObject.material instanceof Array) {
-      sceneObject.material.forEach(material => material.dispose());
+      sceneObject.material.forEach((material) => material.dispose());
     } else {
-        sceneObject.material.dispose();
+      sceneObject.material.dispose();
     }
   }
 
@@ -188,9 +239,7 @@ function removeObject(sceneObject){
 }
 
 //Remove the cubes
-function removeCubes(){
-
-}
+function removeCubes() {}
 
 //RESPONSIVE
 function handleResize() {
@@ -202,22 +251,79 @@ function handleResize() {
   renderer.render(scene, camera);
 }
 
+//RAYCAST
+function onPointerMove(event) {
+  if (selectedObject) {
+    if (selectedObject !== transformedObject) {
+      selectedObject.material.color.set("#69f");
+    }
+    selectedObject = null;
+  }
+
+  pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+  pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+  raycaster.setFromCamera(pointer, camera);
+
+  const intersects = raycaster.intersectObject(group, true);
+
+  if (intersects.length > 0) {
+    const res = intersects.filter(function (res) {
+      return res && res.object;
+    })[0];
+
+    if (res && res.object) {
+      selectedObject = res.object;
+      if (selectedObject !== transformedObject) {
+        selectedObject.material.color.set("#f00");
+      }
+    }
+  }
+}
+
+//CLICK
+
+function onClick() {
+  if (selectedObject) {
+    transformedObject = selectedObject;
+    transformedObject.material.color.set("#ff0");
+    parameters.width = transformedObject.geometry.parameters.width;
+    parameters.height = transformedObject.geometry.parameters.height;
+    parameters.depth = transformedObject.geometry.parameters.depth;
+    widthController.updateDisplay();
+    heightController.updateDisplay();
+    depthController.updateDisplay();
+  } else if (transformedObject) {
+    transformedObject.material.color.set("#69f");
+    transformedObject = null;
+  }
+}
+
+function onKey() {
+  if (event.key === "Escape" || event.keyCode === 27) {
+    transformedObject.material.color.set("#69f");
+    transformedObject = null;
+  }
+}
+
+function scaleFactor(newValue, oldValue) {
+  return newValue / oldValue;
+}
 
 //ANIMATE AND RENDER
 function animate() {
-	requestAnimationFrame( animate );
- 
+  requestAnimationFrame(animate);
+
   control.update();
- 
-	renderer.render( scene, camera );
+
+  renderer.render(scene, camera);
 }
 //-----------------------------------------------------------------------------------
 // CLASS
 //-----------------------------------------------------------------------------------
 
-
 //-----------------------------------------------------------------------------------
-// EXECUTE MAIN 
+// EXECUTE MAIN
 //-----------------------------------------------------------------------------------
 
 main();
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/threejs_parametric/",
+  base: "/Refurnished-Builder/",
   build: {
     sourcemap: true,
-  }
-})
+  },
+});
